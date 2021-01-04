fc4f439e3eb47c3c0e27821bad307d2ca6ccbfb1
sathishpranav <67203862+sathishpranav@users.noreply.github.com>
2021-01-04 15:53:33 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -4,8 +4,7 @@ pytest -v -s -m "Regression" --html=./Reports/report.html Testcases/ --browser c
 rem edge
 
 pytest -v -s -m "sanity" --html=./Reports/report.html Testcases/ --browser edge
-rem IE
-rem pytest -v -s -m "Regression" --html=./Reports/report.html Testcas
-es/ --browser ie
+
+
 
 
